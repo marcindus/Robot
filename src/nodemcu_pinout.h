f860ee89023ec9212c8b080d5e8e89721d0fffa9
duszek <marcin.dus@gmail.com>
2019-11-04 22:52:05 +0100

    initial commit, configuration

---
+++ src/nodemcu_pinout.h
@@ -0,0 +1,16 @@
+//NodeMCU has weird pin mapping.
+//Pin numbers written on the board itself do not correspond to ESP8266 GPIO pin numbers. We have constants defined to make using this board easier:
+
+static const uint8_t D0   = 16;
+static const uint8_t D1   = 5;
+static const uint8_t D2   = 4;
+static const uint8_t D3   = 0;
+static const uint8_t D4   = 2;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 15;
+static const uint8_t D9   = 3;
+static const uint8_t D10  = 1;
+
+
