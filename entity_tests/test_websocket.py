84434e4cdd2e87be0ffef61a4500d72606af87ba
duszek <marcin.dus@gmail.com>
2020-04-05 00:17:20 +0200

    added logger for entity tests

--- entity_tests/test_websocket.py
+++ entity_tests/test_websocket.py
@@ -3,6 +3,8 @@ import os
 import unittest
 import time, requests
 import websocket
+import serial
+import time
 
 class RobotWebSocket():
     def __init__(self, url):
@@ -28,17 +30,27 @@ class SimpleTest(unittest.TestCase):
         self.robotUrl = "ws://192.168.1.10:81"
         self.commands = { "D" : "0", "F" : "Forward" }
 
+    def serialLogger(test):
+        def wrapper(self):
+            with serial.Serial('/dev/ttyUSB0', 57600, timeout=3) as ser:
+                test(self)
+                filename = "{}_{}.txt".format(time.strftime("%Y%m%d%H%M%S"), test.__name__)
+                with open(filename, 'w') as file:
+                    [file.write(str(elem)+ "\n") for elem in ser.readlines()]
+        return wrapper
+
     def testGetDistance(self):
         with RobotWebSocket(self.robotUrl) as ws:
             ws.send("D")
             result = ws.recv()
         self.assertEqual(result, self.commands["D"]) 
 
+    @serialLogger
     def testRunForward(self):
-        with RobotWebSocket(self.robotUrl) as ws:
-            ws.send("F")
-            result = ws.recv()
-        self.assertEqual(result, self.commands["F"]) 
+            with RobotWebSocket(self.robotUrl) as ws:
+                ws.send("F")
+                result = ws.recv()
+            self.assertEqual(result, self.commands["F"])
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
